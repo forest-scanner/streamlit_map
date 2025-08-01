49005777a76a2f29d756c00545a0585e2e2cedd4
valorizasig <37450247+valorizasig@users.noreply.github.com>
2025-08-01 13:38:06 +0200

    Update 1_🌍_Interactive_Map.py
    Se ha añadido dos wms

--- pages/1_🌍_Interactive_Map.py
+++ pages/1_🌍_Interactive_Map.py
@@ -1,32 +1,52 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
 
-markdown = """
-A Streamlit map template
-<https://github.com/opengeos/streamlit-map-template>
-"""
-
 st.sidebar.title("About")
-st.sidebar.info(markdown)
-logo = "https://i.imgur.com/UbOXYAU.png"
-st.sidebar.image(logo)
+st.sidebar.info("Visualizador con WMS de Madrid")
+st.sidebar.image("https://i.imgur.com/UbOXYAU.png")
 
+st.title("Mapa interactivo con WMS de Madrid")
 
-st.title("Interactive Map")
-
+# Columnas para controles y mapa
 col1, col2 = st.columns([4, 1])
-options = list(leafmap.basemaps.keys())
-index = options.index("OpenTopoMap")
+
+# Diccionario con tus WMS
+wms_layers = {
+    "Cartografía General de Madrid": {
+        "url": "https://sigma.madrid.es/vector/services/MAPAS_BASE/CARTOBASE_LINEAS_WGS84/MapServer/WMSServer",
+        "layers": "0"  # En algunos casos se puede usar "0" como identificador simple
+    },
+    "Mapa de Vegetación (2022)": {
+        "url": "https://georaster.madrid.es/ApolloCatalogWMSpublic/service.svc/get",
+        "layers": "ESTRATOS_VEGETALES_VR2022"
+    }
+}
+
+# Lista de nombres visibles para el selector
+wms_names = list(wms_layers.keys())
 
 with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
+    selected_wms = st.selectbox("Selecciona una capa WMS:", wms_names)
 
 with col1:
-
+    # Crear el mapa interactivo
     m = leafmap.Map(
-        locate_control=True, latlon_control=True, draw_export=True, minimap_control=True
+        locate_control=True,
+        latlon_control=True,
+        draw_export=True,
+        minimap_control=True
     )
-    m.add_basemap(basemap)
+
+    # Extraer la info del WMS seleccionado
+    wms_info = wms_layers[selected_wms]
+    m.add_wms_layer(
+        url=wms_info["url"],
+        layers=wms_info["layers"],
+        name=selected_wms,
+        format="image/png",
+        transparent=True
+    )
+
+    # Renderizar el mapa en Streamlit
     m.to_streamlit(height=700)
+
