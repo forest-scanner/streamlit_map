24037219af1a6eb61522af8aa2468a32a6d71fc4
estefaniator <57503831+estefaniator@users.noreply.github.com>
2025-10-20 14:25:01 +0200

    add CartoDB.Positron_2025-10-20
    add CartoDB.Positron

--- Home.py
+++ Home.py
@@ -35,5 +35,5 @@ st.markdown(markdown)
 
 # Mapa ancho completo
 m = leafmap.Map(minimap_control=True)
-m.add_basemap("OpenTopoMap")
+m.add_basemap("CartoDB.Positron")
 m.to_streamlit(height=500, width=1400)  # Fuerza ancho más grande
